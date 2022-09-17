35cf187c1741dab8c73e34644442248c66304925
Lucas Reis <lucasreis64@gmail.com>
2022-09-17 19:26:41 -0300

    pequena adição na sidebar

--- src/SideBar.js
+++ src/SideBar.js
@@ -40,6 +40,16 @@ export default function SideBar () {
             <div class="sidebar">
                 <Usuario/>
                 {sugestoesMap}
+            <div class="sidebarbottom">
+                <h3>
+                    Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade •
+                    Termos • Localizações • Contas mais relevantes • Hashtags •
+                    Idioma
+                </h3>
+            </div>
+            <div class="sidebarbottomcredits">
+                <h3>© 2021 INSTAGRAM DO FACEBOOK</h3>
+            </div>
             </div>
         </div>
     )
