f0ee7541f53bae679926bb91ca86cb9193edb924
Fulano de Tal <fulanodetal@exemplo.br>
2022-09-19 05:07:30 -0300

    Sugestoes

--- src/SideBar.js
+++ src/SideBar.js
@@ -8,38 +8,12 @@ import Usuario from "./Usuario"
 
 
 export default function SideBar () {
-    
-
-    const sugestoes = [
-        {source: badbivesmemes, user: "bad.vibes.memes", h3: "Segue você"},
-        {source: chibirdart, user: "chibirdart", h3: "Segue você"},
-        {source: razoespara, user: "razoesparaacreditar", h3: "Novo no Instagram"},
-        {source: adorableanimals, user: "adorable_animals", h3: "Segue você"},
-        {source: smallcutecats, user: "smallcutecats", h3: "Segue você"}
-    ]
-
-    const sugestoesMap = sugestoes.map((s) => <RenderSugestoes source = {s.source} user = {s.user} h3 = {s.h3} />)
-
-    function RenderSugestoes (props) {
-        return (
-        <div class="sugestoes">
-            <div>
-            <img src={props.source} alt = ""/>
-                <div>
-                    <h3>{props.user}</h3>
-                    <h3>{props.h3}</h3>
-                </div>
-            </div>
-            <h2>Seguir</h2>
-        </div>
-        )
-    }
 
     return (
         <div class="sidebarembalagem">
             <div class="sidebar">
                 <Usuario/>
-                {sugestoesMap}
+                <Sugestoes/>
             <div class="sidebarbottom">
                 <h3>
                     Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade •
--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -1,3 +1,26 @@
 export default function Sugestoes () {
-    
+    const sugestoes = [
+        {source: badbivesmemes, user: "bad.vibes.memes", h3: "Segue você"},
+        {source: chibirdart, user: "chibirdart", h3: "Segue você"},
+        {source: razoespara, user: "razoesparaacreditar", h3: "Novo no Instagram"},
+        {source: adorableanimals, user: "adorable_animals", h3: "Segue você"},
+        {source: smallcutecats, user: "smallcutecats", h3: "Segue você"}
+    ]
+
+    const sugestoesMap = sugestoes.map((s) => <RenderSugestoes source = {s.source} user = {s.user} h3 = {s.h3} />)
+
+    function RenderSugestoes (props) {
+        return (
+        <div class="sugestoes">
+            <div>
+            <img src={props.source} alt = ""/>
+                <div>
+                    <h3>{props.user}</h3>
+                    <h3>{props.h3}</h3>
+                </div>
+            </div>
+            <h2>Seguir</h2>
+        </div>
+        )
+    }
 }
